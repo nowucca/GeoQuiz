736e45750acd4f54194fdc8930351fc0e15536e6
nowucca <steven.atkinson@GMAIL.COM>
2020-01-10 17:36:14 -0800

    Using ImageButtons completes the challenge.

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,16 +2,16 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var previousButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -35,11 +35,11 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
-            checkAnswer(true);
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener {
-            checkAnswer(false);
+            checkAnswer(false)
         }
 
         previousButton.setOnClickListener {
